f0a8194b67440ec1061e9d520127426db3c50bed
yukako <114702943+y-tom@users.noreply.github.com>
2025-07-27 09:12:14 +0900

    トップページを作成

---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+const http = require('node:http');
+/*const fs = require('node:fs');*/
+const pug = require('pug');
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+
+    switch (req.method) {
+      case 'GET':
+        /*
+        const rs = fs.createReadStream('./form.html');
+        rs.pipe(res);
+        */
+        if (req.url === '/') {
+          res.write('<!DOCTYPE html><html lang="ja"><body>' +
+            '<h1>アンケートフォーム</h1>' +
+            '<a href="/enquetes">アンケート一覧</a>' +
+            '</body></html>');
+        } else if (req.url === '/enquetes') {
+          res.write('<!DOCTYPE html><html lang="ja"><body>' +
+            '<h1>アンケート一覧</h1><ul>' +
+            '<li><a href="/enquetes/yaki-tofu">焼き肉・湯豆腐</a></li>' +
+            '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
+            '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
+            '</ul></body></html>');
+        } else if (req.url === '/enquetes/yaki-tofu'){
+          res.write(
+            pug.renderFile('./form.pug',{
+              path:req.url,
+              firstItem:'焼き肉',
+              secondItem:'湯豆腐'
+            })
+          );
+        } else if (req.url === '/enquetes/rice-bread') {
+          res.write(
+            pug.renderFile('./form.pug',{
+              path:req.url,
+              firstItem:'ごはん',
+              secondItem:'パン'
+            })
+          );
+        } else if (req.url === '/enquetes/sushi-pizza') {
+          res.write(
+            pug.renderFile('./form.pug',{
+              path:req.url,
+              firstItem:'寿司',
+              secondItem:'ピザ'
+            })
+          );
+        }
+        res.end();
+        break;
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData += chunk;
+          })
+          .on('end', () => {
+            const answer = new URLSearchParams(rawData);
+            const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
+            console.info(`[${now}] ${body}`);
+            res.write(
+              `<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`
+            );
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', e => {
+    console.error(`[${new Date()}] Server Error`, e);
+  })
+  .on('clientError', e => {
+    console.error(`[${new Date()}] Client Error`, e);
+  });
+const port = 8000;
+server.listen(port, () => {
+  console.info(`[${new Date()}] Listening on ${port}`);
+});
