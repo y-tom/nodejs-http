1a7ba607d10094f92625d66f8157cef65b288275
yukako <114702943+y-tom@users.noreply.github.com>
2025-08-02 09:05:44 +0900

    Basic認証を追加

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 'use strict';
 const http = require('node:http');
-/*const fs = require('node:fs');*/
 const pug = require('pug');
+const auth = require('http-auth');
+const basic =auth.basic(
+  { realm: 'Enquates Area.'},
+  (username, password, callback) => {
+    callback(username === 'guest' && password === 'xaXZJQmE')
+  }
+)
 const server = http
-  .createServer((req, res) => {
+  .createServer(basic.check((req, res) => {
     const now = new Date();
     console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    if(req.url === '/logout'){
+      res.writeHead(401,{
+        'Content-Type':'text/plain; charset=utf-8'
+      });
+      res.end('ログアウトしました');
+      return;
+    }
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
 
     switch (req.method) {
       case 'GET':
-        /*
-        const rs = fs.createReadStream('./form.html');
-        rs.pipe(res);
-        */
         if (req.url === '/') {
           res.write('<!DOCTYPE html><html lang="ja"><body>' +
             '<h1>アンケートフォーム</h1>' +
@@ -74,14 +83,13 @@ const server = http
       default:
         break;
     }
-  })
+  }))
   .on('error', e => {
     console.error(`[${new Date()}] Server Error`, e);
   })
   .on('clientError', e => {
     console.error(`[${new Date()}] Client Error`, e);
   });
-/*const port = 8000;*/
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.info(`[${new Date()}] Listening on ${port}`);
